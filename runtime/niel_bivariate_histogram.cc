e2b6a8b1786c1eeb6de44db7d0cd319496b183ca
Niel Lebeck <nl35@cs.washington.edu>
2018-02-07 09:55:09 -0800

    Add bin labels and formatting to bivariate histogram

--- runtime/niel_bivariate_histogram.cc
+++ runtime/niel_bivariate_histogram.cc
@@ -1,11 +1,15 @@
 #include "niel_bivariate_histogram.h"
 
 #include <cmath>
+#include <iomanip>
 #include <sstream>
 
 namespace art {
 
 namespace nielinst {
+
+#define BIN_FMT std::defaultfloat << std::setprecision(6)
+#define SCALED_NUM_FMT std::fixed << std::setprecision(3)
 
 BivariateHistogram::BivariateHistogram(int numBinsX, double minX, double maxX,
                                        int numBinsY, double minY, double maxY) {
@@ -68,12 +72,65 @@ long BivariateHistogram::TotalCount() {
 
 std::string BivariateHistogram::Print(bool scaled) {
     long count = BivariateHistogram::TotalCount();
+    double binWidthX = (maxX_ - minX_) / numBinsX_;
+    double binWidthY = (maxY_ - minY_) / numBinsY_;
+
     std::stringstream output;
+
+    // print Y variable bin labels
+    output << BIN_FMT << "X\\Y: ";
+    for (int k = 0; k < numBinsY_ + 2; k++) {
+        int binNumY = k - 1;
+        double binStartY = minY_ + binNumY * binWidthY;
+        double binEndY = minY_ + (binNumY + 1) * binWidthY;
+        output << "[";
+        if (binStartY < minY_) {
+            output << "-inf";
+        }
+        else {
+            output << binStartY;
+        }
+        output << ",";
+        if (binEndY > maxY_) {
+            output << "inf";
+        }
+        else {
+            output << binEndY;
+        }
+        output << "): ";
+    }
+    output << std::endl;
+
     for (int i = 0; i < numBinsX_ + 2; i++) {
+        // print X variable bin label
+        int binNumX = i - 1;
+        double binStartX = minX_ + binNumX * binWidthX;
+        double binEndX = minX_ + (binNumX + 1) * binWidthX;
+        output << BIN_FMT << "[";
+        if (binStartX < minX_) {
+            output << "-inf";
+        }
+        else {
+            output << binStartX;
+        }
+        output << ",";
+        if (binEndX > maxX_) {
+            output << "inf";
+        }
+        else {
+            output << binEndX;
+        }
+        output << "): ";
+
         for (int j = 0; j < numBinsY_ + 2; j++) {
             if (scaled) {
-                double scaledVal = (double)bins_[i][j] / count;
-                output << scaledVal;
+                output << SCALED_NUM_FMT;
+                if (count == 0) {
+                    output << 0;
+                }
+                else {
+                    output << (double)bins_[i][j] / count;
+                }
             }
             else {
                 output << bins_[i][j];
